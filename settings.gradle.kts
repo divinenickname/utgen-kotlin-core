010caf61a696693da367d3bdf022f519ff228668
Alexander Ilinykh <divinenickname@gmail.com>
2024-03-05 06:02:36 +0500

    fix. correct project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@
  * in the user manual at https://docs.gradle.org/7.1.1/userguide/multi_project_builds.html
  */
 
-rootProject.name = "utgen"
+rootProject.name = "utgen-core"
 
 pluginManagement {
     repositories {
