a5bbcb6d6665d3023a5b02eb99863325c0e98d71
Alexander Ilinykh <divinenickname@gmail.com>
2024-03-14 11:19:39 +0500

    Add codeBlock for primitive types

---
+++ src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/AssertPrimitiveMethodCodeBlock.kt
@@ -0,0 +1,18 @@
+package io.github.divinenickname.kotlin.utgen.core.domain.codeblocks
+
+import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.PropertySpec
+import io.github.divinenickname.kotlin.utgen.core.domain.Method
+
+class AssertPrimitiveMethodCodeBlock(private val objProperty: PropertySpec, private val method: Method): CodeBlockObj {
+    override fun codeBlock(): CodeBlock {
+        val blockStr = """
+            val expected:${method.returnValue} = TODO('Add value here')
+            val actual = ${objProperty.name}.${method.name}()
+            
+            Assertions.assertEquals(expected, actual)
+        """.trimIndent()
+
+        return blockStr.let(CodeBlock::of)
+    }
+}
