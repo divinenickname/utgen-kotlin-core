d6225a53529e293c2ab66bf4a542f08c2cb925fb
Alexander Ilinykh <divinenickname@gmail.com>
2024-03-15 19:22:52 +0500

    Different tests have different names (#13)

--- src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/chain/MethodChainProcessor.kt
+++ src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/chain/MethodChainProcessor.kt
@@ -1,9 +1,11 @@
 package io.github.divinenickname.kotlin.utgen.core.domain.codeblocks.chain
 
 import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.PropertySpec
 import io.github.divinenickname.kotlin.utgen.core.domain.Method
 import io.github.divinenickname.kotlin.utgen.core.domain.codeblocks.AssertActualCodeBlock
+import org.junit.jupiter.api.Test
 
 class MethodChainProcessor(private val objProperty: PropertySpec, private val method: Method) {
     private val codeChain = setOf(
@@ -12,11 +14,24 @@ class MethodChainProcessor(private val objProperty: PropertySpec, private val me
         AssertNullChain()
     )
 
-    fun generateCodeBlocks(): Set<CodeBlock> {
-        return codeChain
-            .takeWhile { it.isValid(method) }
-            .map { it.execute(objProperty, method) }
-            .takeIf { it.isNotEmpty() }?.toSet()
-            ?: setOf(AssertActualCodeBlock(objProperty, method).codeBlock())
+    fun generateCodeBlocks(): Set<FunSpec> = codeChain
+        .takeWhile { it.isValid(method) }
+        .map(this::funSpec)
+        .takeIf { it.isNotEmpty() }?.toSet()
+        ?: setOf(defaultFunSpec())
+
+
+    private fun funSpec(codeChain: CodeChain): FunSpec {
+        return FunSpec.builder(codeChain.testMethodName(method))
+            .addAnnotation(Test::class)
+            .addCode(CodeBlock.of("val obj = ${objProperty.initializer}\n\n"))
+            .addCode(codeChain.execute(objProperty, method))
+            .build()
     }
+
+    private fun defaultFunSpec(): FunSpec = FunSpec.builder(method.name)
+        .addAnnotation(Test::class)
+        .addCode(CodeBlock.of("val obj = ${objProperty.initializer}\n\n"))
+        .addCode(AssertActualCodeBlock(objProperty, method).codeBlock())
+        .build()
 }
