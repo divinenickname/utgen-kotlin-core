08a9e45ec9aa234b479a81da2151afc42a04b731
Alexander Ilinykh <divinenickname@gmail.com>
2024-04-01 11:54:35 +0500

    AssertDoesNotThrowChain kdoc

--- src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/chain/AssertDoesNotThrowChain.kt
+++ src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/chain/AssertDoesNotThrowChain.kt
@@ -4,6 +4,9 @@ import com.squareup.kotlinpoet.PropertySpec
 import io.github.divinenickname.kotlin.utgen.core.domain.Method
 import io.github.divinenickname.kotlin.utgen.core.domain.codeblocks.AssertDoesNotThrowCodeBlock
 
+/**
+ * If method returns nothing - it shouldn't throw anything. Golden case for UNIT return type
+ */
 class AssertDoesNotThrowChain(
     objProperty: PropertySpec,
     val method: Method
