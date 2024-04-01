01d56a3b6f45d03b82e821ad9f1d5e3e2b68d52d
Alexander Ilinykh <divinenickname@gmail.com>
2024-04-01 12:52:32 +0500

    ImplementBlock kdoc

--- src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/ImplementBlock.kt
+++ src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/codeblocks/ImplementBlock.kt
@@ -2,6 +2,9 @@ package io.github.divinenickname.kotlin.utgen.core.domain.codeblocks
 
 import com.squareup.kotlinpoet.CodeBlock
 
+/**
+ * Implement codeBlock. Contains simple 'todo' impl block
+ */
 object ImplementBlock: CodeBlockObj {
     override fun codeBlock() = CodeBlock.of("TODO(\"Implement\")\n")
 }
