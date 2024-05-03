4cab3478be3cd4c00482206e0bc259db018a82ca
Alexander Ilinykh <divinenickname@gmail.com>
2024-05-03 12:27:39 +0500

    add require params to method

--- src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/Method.kt
+++ src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/Method.kt
@@ -2,11 +2,13 @@ package io.github.divinenickname.kotlin.utgen.core.domain
 
 class Method(
     val name: String,
-    private val returnValue: String = "Unit"
+    private val returnValue: String = "Unit",
+    private val requireExpression: List<String> = emptyList()
 ) {
 
     fun isNullable(): Boolean = returnValue.trim().last() == '?'
     fun returnValue(): String = returnValue.takeUnless { isNullable() } ?: returnValue.trim().dropLast(1)
+    fun requireExpressions(): List<String> = requireExpression
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
