f1f94a383de42ced61fc1c1bd81f3a60d54ee19f
Alexander Ilinykh <divinenickname@gmail.com>
2024-05-04 12:31:51 +0500

    add require statement check

---
+++ src/main/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/testcase/RequireTestCases.kt
@@ -0,0 +1,26 @@
+package io.github.divinenickname.kotlin.utgen.core.domain.testcase
+
+import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.FunSpec
+import io.github.divinenickname.kotlin.utgen.core.domain.Method
+import io.github.divinenickname.kotlin.utgen.core.domain.kpoet.ObjectProperty
+
+class RequireTestCases(
+    private val objProperty: ObjectProperty,
+    private val method: Method,
+) : TestCase(objProperty, method) {
+
+    override fun canApply(): Boolean = method.requireExpressions().isNotEmpty()
+
+    override fun funSpecs(): Set<FunSpec> = method.requireExpressions()
+        .mapIndexed { index, expression ->
+            TestMethod(
+                testMethodName = "${method.name}_requireStmt${index+1}",
+                propertySpec = objProperty.toPropertySpec(),
+                codeBlock = """
+                    TODO('$expression')
+                    Assertions.assertThrows(IllegalArgumentException::class.java) { obj.result() }
+                """.trimIndent().let(CodeBlock::of)
+            ).funSpec()
+        }.toSet()
+}
---
+++ src/test/kotlin/io/github/divinenickname/kotlin/utgen/core/domain/testcase/RequireTestCasesTest.kt
@@ -0,0 +1,56 @@
+package io.github.divinenickname.kotlin.utgen.core.domain.testcase
+
+import com.squareup.kotlinpoet.CodeBlock
+import com.squareup.kotlinpoet.FunSpec
+import io.github.divinenickname.kotlin.utgen.core.domain.Method
+import io.github.divinenickname.kotlin.utgen.core.domain.kpoet.ObjectProperty
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class RequireTestCasesTest {
+
+    private val objProperty = ObjectProperty("org.example", "MyClass")
+
+    @Test
+    fun canApply_isTrue() {
+        val obj = RequireTestCases(objProperty, Method("methodName", requireExpression = listOf("require(1==1)")))
+
+        val actual = obj.canApply()
+
+        Assertions.assertTrue(actual)
+    }
+
+    @Test
+    fun canApply_isFalse() {
+        val obj = RequireTestCases(objProperty, Method("methodName"))
+
+        val actual = obj.canApply()
+
+        Assertions.assertFalse(actual)
+    }
+
+    @Test
+    fun funSpecsTest() {
+        val obj = RequireTestCases(objProperty, Method("methodName", requireExpression = listOf("require(1==1)", "require(false)")))
+
+        val expected = setOf(
+            FunSpec.builder("methodName_requireStmt1")
+                .addAnnotation(Test::class)
+                .addCode(CodeBlock.of("val obj = MyClass()\n\n"))
+                .addCode("""
+                    TODO('require(1==1)')
+                    Assertions.assertThrows(IllegalArgumentException::class.java) { obj.result() }""".trimIndent().let(CodeBlock::of))
+                .build(),
+            FunSpec.builder("methodName_requireStmt2")
+                .addAnnotation(Test::class)
+                .addCode(CodeBlock.of("val obj = MyClass()\n\n"))
+                .addCode("""
+                    TODO('require(false)')
+                    Assertions.assertThrows(IllegalArgumentException::class.java) { obj.result() }""".trimIndent().let(CodeBlock::of))
+                .build()
+        )
+        val actual = obj.funSpecs()
+
+        Assertions.assertEquals(expected, actual)
+    }
+}
